52c0a0fa80e2763cfec144e664928432b0d9fc8b
Haotian He <haotianh@uw.edu>
2014-03-02 14:33:10 -0800

    c

---
+++ hw7.cmd
@@ -0,0 +1,23 @@
+Executable = ./beamsearch_maxent.sh
+getenv = true
+arguments = "/dropbox/13-14/572/hw7/examples/sec19_21.txt /dropbox/13-14/572/hw7/examples/sec19_21.boundary /dropbox/13-14/572/hw7/examples/m1.txt 0.1.1.sys 0 1 1"
+transfer_executable = false
+Queue
+
+Executable = ./beamsearch_maxent.sh
+getenv = true
+arguments = "/dropbox/13-14/572/hw7/examples/sec19_21.txt /dropbox/13-14/572/hw7/examples/sec19_21.boundary /dropbox/13-14/572/hw7/examples/m1.txt 1.3.5.sys 1 3 5"
+transfer_executable = false
+Queue
+
+Executable = ./beamsearch_maxent.sh
+getenv = true
+arguments = "/dropbox/13-14/572/hw7/examples/sec19_21.txt /dropbox/13-14/572/hw7/examples/sec19_21.boundary /dropbox/13-14/572/hw7/examples/m1.txt 2.5.10.sys 2 5 10"
+transfer_executable = false
+Queue
+
+Executable = ./beamsearch_maxent.sh
+getenv = true
+arguments = "/dropbox/13-14/572/hw7/examples/sec19_21.txt /dropbox/13-14/572/hw7/examples/sec19_21.boundary /dropbox/13-14/572/hw7/examples/m1.txt 3.10.100.sys 3 10 100"
+transfer_executable = false
+Queue
